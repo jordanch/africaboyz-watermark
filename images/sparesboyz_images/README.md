9e1a2cc9df59f66c1382ab7a36c19551201041d8
Jordan Cole Hunt <jordancolehunt@gmail.com>
2019-02-19 21:05:17 +0200

    program reads files in dir and watermarks
    need to figure out how to center mosaic

---
+++ images/sparesboyz_images/README.md
@@ -0,0 +1,1 @@
+Please paste your stock images here in this folder. The program only reads .jpg or .JPG files.
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const gm = require("gm")
+const fs = require("fs")
+const path = require("path")
+const promisify = require("util").promisify
 
-gm()
-  .in("-page", "+0+0")
-  .in("images/base.png")
-  .in("images/watermark2.png")
-  .mosaic()
-  .write("tesOutput.jpg", function(err) {
-    if (err) console.log(err)
+const readDir = promisify(fs.readdir)
+// read files
+
+readDir(path.join(__dirname, "images/sparesboyz_images"))
+  .then((data) => {
+    const images = data
+      .filter((fileName) => typeof fileName === "string")
+      .filter((fileName) => fileName.toLowerCase().endsWith(".jpg"))
+
+    // image manip
+    images.forEach((fileName) => {
+      gm()
+        .in("-page", "+0+0")
+        .gravity("Center")
+        .in(`images/sparesboyz_images/${fileName}`)
+        .in("images/slanted.png")
+        .mosaic()
+        .write(`watermarked_${fileName}.jpg`, function(err) {
+          if (err) throw err
+        })
+    })
   })
+  .catch((err) => {
+    console.error(`Something bad happened: ${err}`)
+  })
+
+// for each file, run the gm functions
+
+// write multiple files
